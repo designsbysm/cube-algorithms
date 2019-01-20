a1210fa3c56317590a9432377e6665cc213494e7
Scott A. Matthews <scott@designsbysm.com>
2019-01-19 17:58:45 -0700

    update algorithms

--- src/index.jsx
+++ src/index.jsx
@@ -10,44 +10,44 @@ const methods = [{
             name: 'Sune',
             image: '3x3x3/oll/corner-sune.jpg',
             algorithms: [
-                'R U R\' U R U2 R\'',
+                '(R U R\' U) (R U2 R\')',
             ],
         }, {
             name: 'Antisune',
             image: '3x3x3/oll/corner-antisune.jpg',
             algorithms: [
-                'R U2 R\' U\' R U\' R\'',
+                'R U2 (R\' U\' R U\') R\'',
             ],
         }, {
             name: 'A',
             image: '3x3x3/oll/corner-a.jpg',
             algorithms: [
-                'R U2 R\' U\' R U R\' U\' R U\' R\'',
+                '(R U2) (R\' U\' R U) (R\' U\' R U\') R\'',
                 'F (R U R\' U\')3 F\'',
             ],
         }, {
             name: 'B',
             image: '3x3x3/oll/corner-b.jpg',
             algorithms: [
-                'R U2 R2 U\' R2 U\' R2 U2 R',
+                'R U2 (R2 U\' R2 U\') R2 U2 R',
             ],
         }, {
             name: 'C',
             image: '3x3x3/oll/corner-c.jpg',
             algorithms: [
-                'R2 D\' R U2 R\' D R U2 R',
+                'R2 D\' (R U2 R\') D (R U2 R)',
             ],
         }, {
             name: 'D',
             image: '3x3x3/oll/corner-d.jpg',
             algorithms: [
-                'r U R\' U\' r\' F R F\'',
+                '(r U R\' U\') (r\' F R F\')',
             ],
         }, {
             name: 'E',
             image: '3x3x3/oll/corner-e.jpg',
             algorithms: [
-                'F\' r U R\' U\' r\' F R',
+                'F\' (r U R\' U\') (r\' F R)',
             ],
         }],
     }, {
@@ -56,13 +56,13 @@ const methods = [{
             name: 'J Perm',
             image: '3x3x3/pll/corner-jperm.jpg',
             algorithms: [
-                'R U R\' F (R U R\' U\') R\' F R2 U\' R\'',
+                '(R U R\') F (R U R\' U\') R\' F (R2 U\' R\')',
             ],
         }, {
             name: 'Cross',
             image: '3x3x3/pll/corner-cross.jpg',
             algorithms: [
-                'R\' U R U\' R\' F\' U\' F R U R\' F R\' F\' R U\' R',
+                '(R\' U R U\') R\' F\' U\' F (R U R\') F R\' F\' (R U\' R)',
                 '(R\' U L\' U2 R U\' L)2 U',
             ],
         }],
