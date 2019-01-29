1bdead15aad9fa9a6cb3be8be5973657c9055b46
Scott A. Matthews <scott@designsbysm.com>
2019-01-29 16:46:36 -0700

    make header compent methods pure

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -10,12 +10,14 @@ class Dropdown extends React.Component {
         dropdownVisible: false,
     };
 
-    getSubItems = group => {
-        return this.props.methods.filter(method =>
-            method.group === group
-        ).map(method =>
-            <li className='nav-subitem' key={method.key} onClick={this.handleMenuHide}><Link to={method.key}>{method.name}</Link></li>
-        );
+    getSubItems = (methods, group, hideMenu) => {
+        return methods
+            .filter(method => method.group === group)
+            .map(method => (
+                <li className="nav-subitem" key={method.key} onClick={hideMenu}>
+                    <Link to={method.key}>{method.name}</Link>
+                </li>
+            ));
     };
 
     handleMenuHide = () => {
@@ -38,13 +40,13 @@ class Dropdown extends React.Component {
 
     render() {
         return <div className='nav-item' onClick={this.handleMenuToggle} onMouseEnter={this.handleMenuShow} onMouseLeave={this.handleMenuHide}>
-            <div className='title'>{this.props.title()}
+            <div className='title'>{this.props.title}
                 <FontAwesomeIcon icon={faAngleDown} />
             </div>
             <ul className={`dropdown ${this.state.dropdownVisible ? 'show' : 'hide'}`}>
                 {['2x2x2', '3x3x3', 'Other'].map(group => [
                     <li className='nav-subitem group' key={group}>{group}</li>,
-                    this.getSubItems(group),
+                    this.getSubItems(this.props.methods, group, this.handleMenuHide),
                 ])}
             </ul>
         </div>;
@@ -54,16 +56,18 @@ class Dropdown extends React.Component {
 const Logo = ({ image }) => <Link to='/'><img className='logo' src={image} alt='SM Logo' /></Link>;
 
 class Nav extends React.Component {
-    title = () =>
-        this.props.methods
-            .filter(method => this.props.url === `/${method.key}`)
+    title = (methods, url) =>
+        methods
+            .filter(method => url === `/${method.key}`)
             .map(method => method.name)
             .slice(0, 1)
             .reduce((accumulator, currentValue) => currentValue, 'Unknown');
 
     render() {
+        const title = this.title(this.props.methods, this.props.url);
+
         return <nav>
-            <Dropdown title={this.title} methods={this.props.methods} />
+            <Dropdown title={title} methods={this.props.methods} />
         </nav>;
     };
 };
@@ -72,7 +76,7 @@ class Header extends React.Component {
     render() {
         return <header>
             <Logo image={this.props.logo} />
-            <Nav methods={this.props.methods} url={this.props.match.url} />
+            <Nav methods={this.props.methods} url={this.props.location.pathname} />
         </header>;
     };
 };
