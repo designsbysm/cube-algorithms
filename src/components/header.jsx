c1d1697ddde83b7cbab079d749ab48a75806efb2
Scott A. Matthews <scott@designsbysm.com>
2019-01-29 07:27:47 -0700

    split header into components

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,7 +6,34 @@ import logo from '../images/logo.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDown } from '@fortawesome/pro-regular-svg-icons';
 
-class Header extends React.Component {
+class Dropdown extends React.Component {
+    getSubItems = group => {
+        return this.props.methods.filter(method =>
+            method.group === group
+        ).map(method =>
+            <li className='nav-subitem' key={method.key} onClick={this.props.doMenuHide}><Link to={method.key}>{method.name}</Link></li>
+        );
+    };
+
+    render() {
+        return <div className='nav-item'>
+            <div className='title' onClick={this.props.doMenuToggle} onMouseEnter={this.props.doMenuShow}>{this.props.title()}
+                <FontAwesomeIcon icon={faAngleDown} />
+            </div>
+            <ul className={`dropdown ${this.props.displayMenu ? 'show' : 'hide'}`}>
+                {['2x2x2', '3x3x3', 'Other'].map(group => [
+                    <li className='nav-subitem group' key={group}>{group}</li>,
+                    this.getSubItems(group),
+                ])}
+            </ul>
+        </div>;
+    };
+};
+
+const Logo = ({ image }) => <Link to='/'><img className='logo' src={image} alt='SM Logo' /></Link>;
+
+// TODO: move handlers to dropdown
+class Nav extends React.Component {
     state = {
         displayMenu: false,
     };
@@ -29,17 +56,9 @@ class Header extends React.Component {
         });
     };
 
-    getGroupedNav = (group, methods) => {
-        return methods.filter(method =>
-            method.group === group
-        ).map(method =>
-            <li key={method.key} onClick={this.handleMenuHide}><Link to={method.key}>{method.name}</Link></li>
-        );
-    };
-
-    render() {
+    title = () => {
         const current = this.props.methods.filter(method => {
-            if (this.props.match.url.endsWith(method.key)) {
+            if (this.props.url.endsWith(method.key)) {
                 return method.name;
             } else {
                 return false;
@@ -51,20 +70,22 @@ class Header extends React.Component {
             name = current[0].name;
         }
 
+        return name;
+    };
+
+    render() {
+        return <nav onMouseLeave={this.handleMenuHide}>
+            <Dropdown title={this.title} methods={this.props.methods} doMenuToggle={this.handleMenuToggle} doMenuShow={this.handleMenuShow} doMenuHide={this.handleMenuHide} displayMenu={this.state.displayMenu} />
+        </nav>;
+    };
+};
+
+class Header extends React.Component {
+    render() {
         return <header>
-            <Link to='/'><img className='logo' src={logo} alt='SM Logo' /></Link>
-            <nav onMouseLeave={this.handleMenuHide}>
-                <div className='title' onClick={this.handleMenuToggle} onMouseEnter={this.handleMenuShow}>{name}
-                    <FontAwesomeIcon icon={faAngleDown} />
-                </div>
-                <ul className={this.state.displayMenu ? 'show' : 'hide'}>
-                    {['2x2x2', '3x3x3', 'Other'].map(group => [
-                        <li className='group' key={group}>{group}</li>,
-                        this.getGroupedNav(group, this.props.methods),
-                    ])}
-                </ul>
-            </nav>
-        </header >;
+            <Logo image={logo} />
+            <Nav methods={this.props.methods} url={this.props.match.url} />
+        </header>;
     };
 };
 
--- src/methods/2x2x2/cases/basic.js
+++ src/methods/2x2x2/cases/basic.js
@@ -2,6 +2,6 @@ export default [{
     key: '2x2x2/basic/last',
     name: 'Last',
     algorithms: [
-        '(R\' U R\') D2 (R U\' R\') DR R2',
+        '(R\' U R\') D2 (R U\' R\') D2 R2',
     ],
 }];
