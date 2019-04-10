94428322f0ed60f8489124287cac6fc61b52954e
Scott A. Matthews <scott@designsbysm.com>
2019-04-10 08:45:03 -0700

    update server config check

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
+require("dotenv")
+  .config();
+
+const environment = process.env.SERVER_ENV;
+const port = process.env.SERVER_PORT;
+
+const fs = require("fs");
+if (!fs.existsSync("./.env") || !environment || !port) {
+  console.error("Exiting: missing .env config/options");
+  process.exit(1);
+}
+
 const express = require("express");
 const path = require("path");
 const app = express();
 
-// load .env variables
-const dotenv = require("dotenv");
-dotenv.config();
-const config = {
-  environment: process.env.SERVER_ENV,
-  port: process.env.SERVER_PORT,
-};
-
-// setup server
 app.use(express.static(path.join(__dirname, "build")));
-
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-// launch server
-app.listen(config.port, () => {
-  console.info("%s server listening on %d", config.environment, config.port);
+app.listen(port, () => {
+  console.info("%s server listening on %d", environment, port);
 });
