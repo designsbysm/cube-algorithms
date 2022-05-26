3a8ac599f76402625cb6f5c591fa1a56425e8791
Scott A. Matthews <scott@designsbysm.com>
2022-05-26 10:22:51 -0700

    replace rn Dimensions with useWindowDimensions

--- src/components/Cases.jsx
+++ src/components/Cases.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import { Dimensions, FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, useWindowDimensions } from 'react-native';
 
 import Case from './Case';
 
 const CasesComponent = ({ cases }) => {
-  const window = Dimensions.get('window').width;
+  const { width } = useWindowDimensions();
 
   let columns = 2;
-  if (window >= 1400) {
+  if (width >= 1400) {
     columns = 7;
-  } else if (window >= 1200) {
+  } else if (width >= 1200) {
     columns = 6;
-  } else if (window >= 992) {
+  } else if (width >= 992) {
     columns = 5;
-  } else if (window >= 768) {
+  } else if (width >= 768) {
     columns = 4;
-  } else if (window >= 576) {
+  } else if (width >= 576) {
     columns = 3;
   }
 
-  const tile = (window - 30) / columns;
+  const tile = (width - 30) / columns;
   const styles = createStyles(tile);
 
   return (
--- src/components/Stages.jsx
+++ src/components/Stages.jsx
@@ -1,19 +1,9 @@
 import React, { useLayoutEffect, useState } from 'react';
-import useEvent from '../hooks/useEvent';
 import { Dimensions, ScrollView, StyleSheet } from 'react-native';
 
 import Stage from './Stage';
 
 const StagesComponent = ({ method }) => {
-  const [
-    width,
-    setWidth,
-  ] = useState(0);
-  useEvent('resize', () => {
-    const window = Dimensions.get('window').width;
-    setWidth(window);
-  });
-
   return (
     <ScrollView>
       {method.map((item, index) => {
